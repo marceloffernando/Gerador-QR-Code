e8384f9023c857e86ccfb2e421fb4549bb5ea3c3
Marcelo Fernando <marceloffernando8@gmail.com>
2024-09-20 10:30:27 -0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(olar)
+
+const container =document.querySelector(".container")
+const qrCodeBtn = document.querySelector(".qr-form button")
+
+const qrCodeInput = document.querySelector(".qr-form input")
+
+// EVENTOS
+function gerarQrCode(){
+    console.log()
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    gerarQrCode();
+  });
